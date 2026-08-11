b6b7bfebaaab41392726e2b7b80f1be94dbc548f
ussmarines <ussmarines@gmail.com>
2026-08-11 11:39:08 +0200

    security: harden CSV formula neutralization

--- includes/class-pixcensus-csv.php
+++ includes/class-pixcensus-csv.php
@@ -11,13 +11,19 @@ final class PIXCENSUS_CSV {
 	/**
 	 * Neutralize values that spreadsheet applications may interpret as formulas.
 	 *
+	 * Treat ASCII controls, Unicode whitespace, byte-order marks, zero-width
+	 * characters and bidi/formatting controls as ignorable leading characters.
+	 * This prevents a formula marker from being hidden behind characters that
+	 * spreadsheet applications may trim or render invisibly.
+	 *
 	 * @param mixed $value Cell value.
 	 * @return string
 	 */
 	public static function neutralize_formula( $value ): string {
 		$pixcensus_value = (string) $value;
+		$pixcensus_formula_pattern = '/^[\x00-\x20\x7F\x{0085}\x{00A0}\x{1680}\x{2000}-\x{200F}\x{2028}-\x{202F}\x{205F}\x{2060}-\x{206F}\x{3000}\x{FEFF}]*[=+\-@]/u';
 
-		if ( preg_match( '/^[\x00-\x20]*[=+\-@]/', $pixcensus_value ) || preg_match( '/^[\t\r\n]/', $pixcensus_value ) ) {
+		if ( preg_match( $pixcensus_formula_pattern, $pixcensus_value ) || preg_match( '/^[\t\r\n]/', $pixcensus_value ) ) {
 			return "'" . $pixcensus_value;
 		}
 
